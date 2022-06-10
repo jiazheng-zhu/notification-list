caf67f7abd60207bc3316463367487842f1a6693
jiazheng-zhu <jiazheng999@gmail.com>
2022-06-10 17:01:29 +0200

    first

---
+++ src/components/NotificationList.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+   <SingleNotification v-for="(notification,index) in notifications" v-bind:key="index" :notifDetails="notification"/>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import SingleNotification from '@/components/SingleNotification.vue'
+
+
+export default {
+  name: 'NotificationList',
+  components:{
+    SingleNotification
+  },
+  data(){
+    return{
+      notifications: []
+    }
+  },
+  beforeMount(){
+    axios
+    .get('https://api.npoint.io/87f44ba28aa5a5fba21d')
+    .then(response => {
+     console.log(response.data.notifications);
+      this.notifications = response.data.notifications;
+    })
+  }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/SingleNotification.vue
@@ -0,0 +1,46 @@
+<template>
+    <div calss="notification info" :class="{
+    info: notifDetails.type== 'info',
+    warning:notifDetails.type=='warning',
+    urgent:notifDetails.type== 'urgent',
+    readStatus: notifiDetails.read_status == true,}">
+        <p>{{ notifDetails.description }}</p> 
+        <p>{{ TimeSet(notifiDetails.timestamp) }}</p>
+    </div>
+</template>
+
+<script>
+import timeMoment from "timeMoment";
+export default {
+  name: "SingleNotification",
+  props: {
+    notifiDetails: Object,
+  },
+  methods: {
+    TimeSet: function (value) {
+      return timeMoment.unix(value).format("DD/MM - hh:mm");
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+    .notification{
+        padding:5px;
+        box-sizing:border-box;
+}
+    .info {
+        border-left:3px solid lightskyblue;
+    }
+    .warning {
+        border-left:3px solid orange;
+    }
+    .urgent {
+        border-left:3px solid red;
+    }
+    
+</style>
+
+
+
+
